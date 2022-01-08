1ab097fb1c842d5cf79f59878031e14f8e76f4a3
NMontesino <namontesino@gmail.com>
2022-01-08 08:27:09 -0500

    Added Persistent Header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import Homepage from './pages/homepage/Homepage'
 import Shop from './pages/shop/Shop'
+import Header from './components/header/Header'
 
 import './App.css'
 
@@ -10,6 +11,7 @@ import './App.css'
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path='/' component={Homepage} />
 				<Route path='/shop' component={Shop} />

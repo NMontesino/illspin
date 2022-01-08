704ce1a5cdc303d75582eee3658525eecf540b2b
NMontesino <namontesino@gmail.com>
2022-01-08 08:03:20 -0500

    Added Shop Page w/ Collection Preview and Collection Items

---
+++ src/components/collection-preview/CollectionPreview.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import CollectionItem from '../collection-item/CollectionItem'
+
+import './CollectionPreview.scss'
+
+
+const CollectionPreview = ({ title, items }) => {
+	return (
+		<div className='collection-preview'>
+			<h1 className='title'>{title.toUpperCase()}</h1>
+			<div className='preview'>
+				{items
+					.filter((item, idx) => {
+						return idx < 4
+					})
+					.map(({id, ...otherItemProps}) => {
+						return <CollectionItem key={id} {...otherItemProps} />
+					})}
+			</div>
+		</div>
+	)
+}
+
+
+export default CollectionPreview
